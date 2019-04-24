409ba5523ad2258290f2a134c53dd34b0322de66
dmu 08 <dmumac08@dmus-Mac-mini.local>
2019-04-24 11:00:11 +0200

    most functions for firebase is done

--- IOSProject/IOSProject/Firebase.swift
+++ IOSProject/IOSProject/Firebase.swift
@@ -11,17 +11,56 @@ import Firebase
 import FirebaseDatabase
 import CoreLocation
 
-class Firebase {
+class FirebaseService {
+    var database = Database.database()
     var reference = Database.database().reference()
     
-    let testData = Data(coordinate: CLLocationCoordinate2DMake(0.0, 0.0), ph: 10, moisture: 18, temperature: 7.9, id: 37594395348)
+    var dataList: [Data]
+    
+    init() {
+        dataList = []
+        reference.observe(DataEventType.childAdded) { (snapshot) in
+            self.dataList.append(self.firebaseToData(snapshot))
+            print("Got snapshot")
+            print(self.dataList.count)
+            self.getDataList(ids: [123])
+        }
+    }
+    
+    let testData = Data(coordinate: CLLocationCoordinate2DMake(0.0, 0.0), ph: 10, moisture: 18, temperature: 8, id: 37594395348, date: Date.init())
+    
+    func pushTestData(){
+        pushDataObject(data: testData)
+    }
     
     func pushDataObject(data: Data){
+        reference.childByAutoId().setValue(data.asDictionary())
         //Incomplete Function
     }
     
-    func getDataList(ids: [Int]) -> [Data] {
+    func firebaseToData(_ snapshot: DataSnapshot) -> Data {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let dataDict = snapshot.value as? [String: AnyObject] ?? [:]
+        
+        let coordinate = CLLocationCoordinate2D.init(latitude: dataDict["Latitude"] as! CLLocationDegrees,longitude: dataDict["Longitude"] as! CLLocationDegrees)
+        let date = formatter.date(from: dataDict["Date"] as! String)
+        
+        let temperature = dataDict["Temperature"] as! Int
+        let ph = dataDict["PH"] as! Int
+        let moisture = dataDict["Moisture"] as! Int
+        let id = dataDict["ID"] as! Int
+        
+        
+        return Data(coordinate: coordinate, ph: ph, moisture: moisture, temperature: temperature, id: id, date: date!)
+    }
+    
+    func getDataList(ids: [Int]) -> [Data]? {
+        print("Get DataList")
         //Incomplete Function
-        return [testData]
+        let result = dataList
+        print(result as Any)
+        return result
     }
 }
